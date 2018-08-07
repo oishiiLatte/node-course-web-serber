b049a9d5ee43c7cd7d208fee0c82130a962570fb
Krisna Adi <obbiy@Krisnas-MacBook-Pro.local>
2018-08-07 23:10:09 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`
+
+    fs.appendFile('server.log', `${log}\n`, (err) => {
+        if(err) {
+            console.log('unable to save log')
+        }
+    })
+    next()
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
+
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>')
+    res.render('home.hbs', {
+        page_title: 'Home Page',
+        welcome_message: 'Welcome dude',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        page_title: 'About Page',
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+})
